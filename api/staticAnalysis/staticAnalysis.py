d3f02b4939eeb7dceceb7855f2a4b5597e8560c3
devlongen <iago.mendonca_2005@hotmail.com>
2025-05-13 12:58:46 -0300

    refactor: refactor function mean

--- api/staticAnalysis/staticAnalysis.py
+++ api/staticAnalysis/staticAnalysis.py
@@ -1,50 +1,44 @@
 import pandas as pd
-import numpy as np
+import json
 csv = pd.read_csv("/home/iagolongen/Downloads/student_habits_performance.csv")
 class staticAnalysis:
     def __init__(self,df):
         self.df = pd.DataFrame(df) # get df the front
         self.lengthrow_dataframe,self.lengthcol_dataframe=self.df.shape # use pandas.shape(module)
-        self.mean_dataframe_col=dict # create function mean and for col
-        self.sum_dataframe_col=dict # create function sum and for col
-        self.min_dataframe_col=dict # create function min and for col
-        self.max_dataframe_col=dict # create function max and for col
-        self.mfv_dataframe_col=dict # create function modeest
-        self.median_dataframe_col=dict # create function
-        self.cov_dataframe_col=dict # create function
-        self.cor_dataframe_col=dict # create function
+        self.mean_dataframe_col=json # create function mean and for col
+        self.sum_dataframe_col=json # create function sum and for col
+        self.min_dataframe_col=json # create function min and for col
+        self.max_dataframe_col=json # create function max and for col
+        self.mfv_dataframe_col=json # create function modeest
+        self.median_dataframe_col=json # create function
+        self.cov_dataframe_col=json # create function
+        self.cor_dataframe_col=json # create function
 
-    def lenghtDataframe(self) -> dict:
-        dict_length = {"length_row":self.lengthrow_dataframe,"length_col":self.lengthcol_dataframe}
-        return dict_length
+    def lenghtDataframe(self) -> json:
+        json_length = {"length_row":self.lengthrow_dataframe,"length_col":self.lengthcol_dataframe}
+        return json_length
     
-    def mean_dataframe(self,qtd_row,qtd_column) -> dict:
-        sum_value=0
-        for i in range(qtd_row):
-            value = self.df.iloc[i]
-            if value is int:
-                sum_value = sum_value + value
-            else:
-                pass
-        mean_var = sum_value/qtd_row
-        dict_mean = {"mean":mean_var}
-            
-        print(dict_mean)
-        return dict_mean
+    def mean_dataframe(self) -> json:
+        df_tratament = self.df
+        qtd_row=self.lengthrow_dataframe
+        for col in df_tratament:
+            for value in range(qtd_row):
+                test=df_tratament.loc[value,col]
+        return None
         
-    def sum_dataframe(self,qtd_row,qtd_column) -> dict:
+    def sum_dataframe(self,qtd_row,qtd_column) -> json:
         return None
-    def min_dataframe(self,qtd_row,qtd_column) -> dict:
+    def min_dataframe(self,qtd_row,qtd_column) -> json:
         return None
-    def max_dataframe(self,qtd_row,qtd_column) -> dict:
+    def max_dataframe(self,qtd_row,qtd_column) -> json:
         return None
-    def mfv_dataframe(self,qtd_row,qtd_column) -> dict:
+    def mfv_dataframe(self,qtd_row,qtd_column) -> json:
         return None
-    def median_dataframe(self,qtd_row,qtd_column) -> dict:
+    def median_dataframe(self,qtd_row,qtd_column) -> json:
         return None
-    def cov_dataframe(self,qtd_row,qtd_column) -> dict:
+    def cov_dataframe(self,qtd_row,qtd_column) -> json:
         return None
-    def cor_dataframe(self,qtd_row,qtd_column) -> dict:
+    def cor_dataframe(self,qtd_row,qtd_column) -> json:
         return None
         
         
